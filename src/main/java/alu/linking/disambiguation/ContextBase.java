c71dc7b6e1ad74f549137594cdc9ce1d95af3cad
kmdn <kmdn@users.noreply.github.com>
2018-12-23 20:13:42 +0100

    Added PageRankPicker
    Added a cluster scoring scheme using pagerank on contextual subgraphs to score how well each node within the graph should be connected based on its embedding's cosine sim

--- src/main/java/alu/linking/disambiguation/ContextBase.java
+++ src/main/java/alu/linking/disambiguation/ContextBase.java
@@ -5,6 +5,11 @@ import java.util.Collection;
 public interface ContextBase<M> {
 	public void linkContext(Collection<M> context);
 
+	/**
+	 * Generally meant for some stuff that should be executed upon context changing,
+	 * e.g. when something should be computed only once and not every time an item
+	 * is to be scored
+	 */
 	public void updateContext();
 
 }
