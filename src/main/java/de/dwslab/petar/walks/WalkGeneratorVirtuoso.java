558f34320956fbcb2cfa2686fc9598c5278e552f
Kris! <kmdn@users.noreply.github.com>
2019-09-04 11:43:08 +0200

    Update WalkGeneratorVirtuoso.java
    Added code stubs for WalkGeneratorVirtuoso for compilation

--- src/main/java/de/dwslab/petar/walks/WalkGeneratorVirtuoso.java
+++ src/main/java/de/dwslab/petar/walks/WalkGeneratorVirtuoso.java
@@ -1,15 +1,58 @@
 package de.dwslab.petar.walks;
 
-/*
+
 import java.util.Collection;
 
 import org.apache.jena.query.QueryExecution;
 
-import virtuoso.jena.driver.VirtGraph;
-import virtuoso.jena.driver.VirtuosoQueryExecution;
-import virtuoso.jena.driver.VirtuosoQueryExecutionFactory;
+//import virtuoso.jena.driver.VirtGraph;
+//import virtuoso.jena.driver.VirtuosoQueryExecution;
+//import virtuoso.jena.driver.VirtuosoQueryExecutionFactory;
 
 public class WalkGeneratorVirtuoso extends WalkGenerator {
+
+	public WalkGeneratorVirtuoso(Collection<String> predicateBlacklist, String entityQueryStr, String logEntities,
+			WalkResultProcessor resultProcessor, String entitiesOutputPath) {
+		super(predicateBlacklist, entityQueryStr, logEntities, resultProcessor, entitiesOutputPath);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected QueryExecution queryCreate(String query) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void beginREAD() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void beginWRITE() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void endTransaction() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected boolean isLineByLineOutput() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void close() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+/*
 	private final VirtGraph virtGraph;
 
 	public WalkGeneratorVirtuoso(VirtGraph virtGraph, Collection<String> predicateBlacklist, String entityQueryStr,
@@ -54,5 +97,5 @@ public class WalkGeneratorVirtuoso extends WalkGenerator {
 		return true;
 	}
 
-}
 */
+}
